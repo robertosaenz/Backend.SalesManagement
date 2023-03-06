03ebf6eac1493c005a6ef5cea5aef2a60a96448e
GMDSA\rsaenz <rsaenz@canvia.com>
2023-03-06 10:30:29 -0500

    Create class Sales

---
+++ Backend.SalesManagement.Models/Sales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.SalesManagement.Models
+{
+    public class Sales
+    {
+        public string Id { get; set; }
+
+        public string UserAccountId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime LastUpdatedDateTime { get; set; }
+    }
+}
