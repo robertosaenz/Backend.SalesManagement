13b6f056b956cf9b06dda30a43a1b7396d3a1dca
GMDSA\rsaenz <rsaenz@canvia.com>
2023-03-06 16:07:50 -0500

    Config and Update API REST

---
+++ Backend.SalesManagement/Controllers/SalesController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Backend.SalesManagement.Models;
+using Backend.SalesManagement.Services.Interfaces;
+using Backend.SalesManagement.Validations;
+
+namespace Backend.SalesManagement.Controllers
+{
+    [Route("api/sales")]
+    [ApiController]
+    public class SalesController : Controller
+    {
+        private readonly ISalesService _salesService;
+
+        public SalesController(ISalesService salesService)
+        {
+            this._salesService = salesService;
+        }
+
+        [HttpPost("create")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Create([FromBody] Sales sales)
+        {
+            if (sales.IsValid(out IEnumerable<string> errors))
+            {
+                var result = await _salesService.Create(sales);
+
+                return CreatedAtAction(
+                    nameof(GetAllByUserAccountId),
+                    new { id = result.UserAccountId }, result);
+            }
+            else
+            {
+                return BadRequest(errors);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetAllByUserAccountId(string id)
+        {
+            if (!String.IsNullOrEmpty(id))
+            {
+                var result = _salesService.GetAllByUserAccountId(id);
+
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("update")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update([FromBody] Sales sales)
+        {
+            if (sales.IsValid(out IEnumerable<string> errors))
+            {
+                var result = await _salesService.Update(sales);
+
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(errors);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetAll()
+        {
+            var result = _salesService.GetAll();
+
+            return Ok(result);
+        }
+
+        [HttpPost("delete/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (!String.IsNullOrEmpty(id))
+            {
+                var result = await _salesService.Delete(id);
+
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
--- Backend.SalesManagement/Startup.cs
+++ Backend.SalesManagement/Startup.cs
@@ -1,15 +1,15 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Backend.SalesManagement.Context;
+using Backend.SalesManagement.Repositories;
+using Backend.SalesManagement.Repositories.Interfaces;
+using Backend.SalesManagement.Services;
+using Backend.SalesManagement.Services.Interfaces;
 
 namespace Backend.SalesManagement
 {
@@ -20,31 +20,65 @@ namespace Backend.SalesManagement
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public static IConfiguration Configuration { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            ConfigureTransientServices(services);
+            ConfigureRepositories(services);
+            ConfigureEntityFramework(services);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        private static void ConfigureTransientServices(IServiceCollection services)
+        {
+            services.AddTransient<ISalesService, SalesService>();
+        }
+
+        private static void ConfigureRepositories(IServiceCollection services)
+        {
+            services.AddSingleton<ISalesRepository, SalesRepository>();
+        }
+
+        private static void ConfigureEntityFramework(IServiceCollection services)
+        {
+            var databaseName = Configuration["EntityFramework:DatabaseName"];
+
+            services.AddDbContext<SalesDatabaseContext>(options =>
+                options.UseInMemoryDatabase(databaseName));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllers();
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
