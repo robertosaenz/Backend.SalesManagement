5be8a6f9c2a8ee87d38dc36f799196719ce98bcf
GMDSA\rsaenz <rsaenz@canvia.com>
2023-03-06 10:32:05 -0500

    Create class sales database context

---
+++ Backend.SalesManagement.Context/SalesDatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Backend.SalesManagement.Models;
+//using Microsoft.EntityFrameworkCore;
+
+namespace Backend.SalesManagement.Context
+{
+    public class SalesDatabaseContext 
+    {
+    }
+}
